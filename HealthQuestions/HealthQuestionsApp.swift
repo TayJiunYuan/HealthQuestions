25799ba2e8d5a1dc320af932e834da541e2c353a
TayJiunYuan <tayjy2000@gmail.com>
2025-06-03 19:11:51 +0800

    Initial Commit

---
+++ HealthQuestions/HealthQuestionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthQuestionsApp.swift
+//  HealthQuestions
+//
+//  Created by Tay Jiun Yuan on 3/6/25.
+//
+
+import SwiftUI
+
+@main
+struct HealthQuestionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
